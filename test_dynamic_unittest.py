1f83006de5fa6dd669e46b3a2f6a0161cad07f49
rich-hart <rich.hart@rice.edu>
2015-11-24 22:30:22 +0000

    adding useful unittest examples

---
+++ test_dynamic_unittest.py
@@ -0,0 +1,19 @@
+import unittest
+
+l = [["page_1", "a", "a",], ["page_2", "a", "b"], ["page_3", "b", "b"]]
+
+class TestSequense(unittest.TestCase):
+    pass
+
+def test_generator(a, b):
+    def test(self):
+        self.assertEqual(a,b)
+    return test
+
+if __name__ == '__main__':
+    for t in l:
+        test_name = 'test_%s' % t[0]
+        test = test_generator(t[1], t[2])
+        setattr(TestSequense, test_name, test)
+    unittest.main()
+
